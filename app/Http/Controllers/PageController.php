e33c6664b883168fc8d7913de406d7e122069783
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-20 18:07:24 +0200

    added controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Movie;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $arrayMovie = Movie::all();
+        return view("index", compact("arrayMovie"));
+    }
+}
